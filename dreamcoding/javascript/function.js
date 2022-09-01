c25e0e9a9b295ca46d9ba87a66606f5096cfc7a6
SO HUI <haejuuun925@gmail.com>
2022-09-01 20:05:38 +0900

    9/1 study

--- dreamcoding/javascript/function.js
+++ dreamcoding/javascript/function.js
@@ -157,3 +157,26 @@ const simpleMultiply = (a, b) => {
 (function hello() {
     console.log('IIFE');
 })();
+
+
+// Fun quiz time !
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, reminder
+
+function calculate(command, a, b) {
+    switch (command) {
+        case 'add':
+            return a + b;
+        case 'substract':
+            return a - b;
+        case 'divide':
+            return a / b;
+        case 'multiply':
+            return a * b;
+        case 'reminder':
+            return a % b;
+        default:
+            throw Error('unknown command');
+    }
+}
+console.log(calculate('add', 2, 3));
