a0f1b07690b69404444fc2111164f025c9c55320
SO HUI <haejuuun925@gmail.com>
2023-01-07 15:14:27 +0900

    20230107 study

---
+++ dreamcoding/minigame/shopping.js
@@ -0,0 +1,51 @@
+// json 파일의 아이템들 받아오기
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+  // Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+  // Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+    return;  // key, value 값 null일 경우 아무것도 처리하지 않고 함수를 끝낸다
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+  // main
+loadItems()
+    .then(items => {
+    displayItems(items);
+    setEventListeners(items);
+    })
+    .catch(console.log);
