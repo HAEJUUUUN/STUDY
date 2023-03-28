61a89dc90238a0e38eaffd47a8dec96e961bf02c
SO HUI <haejuuun925@gmail.com>
2023-03-28 13:13:36 +0900

    venture finals

--- venture-startup-academy/test/20230329-finals/js/clock.js
+++ venture-startup-academy/test/20230329-finals/js/clock.js
@@ -2,11 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
+    
     // padStart(2, "0"): 문자를 무조건 두자리수로 설정한 뒤 빈 곳은 0으로 채우기
     // String으로 감싸서 숫자 --> 문자열
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
+    
     // clock에 텍스트형태로 데이터 값 집어넣기
     clock.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
@@ -14,7 +16,4 @@ function getClock() {
 // setInterval(실행하고 싶은 함수, 몇 초 간격) 5000ms --> 5초
 // web site가 load되자마자 getClock()을 실행하고 매초마다 실행되도록
 getClock();
-setInterval(getClock, 1000);  
-
-// setInterval은 5초마다, setTimeout은 5초 뒤 실행한 후 끝.
-// setTimeout(sayHello, 5000);
+setInterval(getClock, 1000);
--- venture-startup-academy/test/20230329-finals/js/todo.js
+++ venture-startup-academy/test/20230329-finals/js/todo.js
@@ -7,18 +7,22 @@ const TODOS_KEY = "todos";
 let toDos = [];
 
 function saveToDos() {
+
     // toDos array의 내용을 localStorage에 넣기
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 } 
 
 function deleteToDo(event) {
+
     /* ❌버튼 클릭 시 event를 얻고 해당 event는 target을 준다 
     -> 여기서 target은 button이며 그 button은 li라는 부모를 가진다
     -> parentElement를 통해 해당 button의 부모요소에 접근한다
     */
     const li = event.target.parentElement;
+
     // 부모 요소 제거
     li.remove();
+
     /* toDo는 toDos DB에 있는 요소 중 하나. 
     클릭한 ❌버튼 li.id와 다른 toDo는 남겨두도록 그래서 ❌버튼이 클릭된 목록을 지우도록 */    
     toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
@@ -27,16 +31,20 @@ function deleteToDo(event) {
 }
 
 function paintToDo(newTodo) {
+
     // li element 생성
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
+
     // button 텍스트 변경
     button.innerText = "❌";
+
     // ❌button클릭시 deleteToDo 함수 작동
-    button.addEventListener("click", deleteToDo)
+    button.addEventListener("click", deleteToDo);
+
     // li태그안에 span태그 넣어줌
     li.appendChild(span);
     li.appendChild(button);
@@ -45,15 +53,18 @@ function paintToDo(newTodo) {
 
 function handleToDoSubmit(event) {
     event.preventDefault();
+
     // newToDo: input의 value를 비우기 전의 값을 나타내는 string
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+
     // object로 저장
     const newToDoObj = {
         text: newTodo,
         id: Date.now(),
     };    
     toDos.push(newToDoObj);
+
     // newTodo를 paintToDo에 넣어서 호출 --> newToDoObj object를 넣는 것으로 바뀜
     paintToDo(newToDoObj);
     saveToDos();
@@ -64,13 +75,16 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos !== null) {
+
     /* JSON.parse()로 string data type을 object로 바꿈 
     이 Object는 Array 형태로 바뀜. 
     즉 index를 통해 value를 access 할 수 있게됨 */
     const parsedToDos = JSON.parse(savedToDos);
+
     /* localStorage에 toDo 데이터 값들이 있을 시 toDos에 parsedToDos를 넣어 과거의 toDo들을 복원
     이 과정이 없을시 toDos는 빈 array로 시작하기 때문에 localStorage에서 과거의 toDo가 사라지고 새로 작성하는 toDo만 추가됨*/
     toDos = parsedToDos;
+    
     // forEach --> array의 각 item에 대해 function을 실행하게 한다
     parsedToDos.forEach(paintToDo);
 }
